9dd16c12ec14c426da13e70faf9961c6e309aa50
Vinay Kumar <88894702+v-codey@users.noreply.github.com>
2022-06-21 22:50:19 +0530

    update daily project
    still pending some things

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var allStudentDetails = {
     bca: []
 }
 
-class StudentData  {
+class StudentData {
     readDetails() {
         this.name = document.querySelector("#sName").value;
         this.roll = document.querySelector("#sRoll").value;
@@ -14,7 +14,6 @@ class StudentData  {
         this.gender = document.querySelector("input[name=gender]:checked").value;
         this.course = document.querySelector("#sCourse").value;
         this.course = this.course.toLowerCase();
-        
         this.addStudentDetailInObject(this.course);
     }
 
@@ -34,15 +33,51 @@ class StudentData  {
             case "btech":
                 allStudentDetails.btech.push(newStu);
                 break;
-        }        
-        console.log(allStudentDetails);
+            }        
+            console.log(allStudentDetails);
+        updateTable();
     }
-
+;
 }
+
+
 
 document.querySelector("#sub_btn").addEventListener('click', () => {
     var newStud1 = new StudentData();
     newStud1.readDetails();
 });
+
+var uploadBtn = document.querySelector('#uploadDataBtn');
+
+uploadBtn.addEventListener('click', ()=>{
+    var uploadDataSection = document.querySelector(".uploadContainer");
+    uploadDataSection.classList.toggle("hide");
+    if(uploadDataSection.classList.contains("hide")){
+        uploadBtn.value = "Upload Student Data";
+    }else{
+        uploadBtn.value = "Hide Upload Data";
+    }
+});
+
+
+var updateTable = ()=>{
+    var tableBody = document.querySelector('.divTableBody');
+    var tableRow = document.createElement('div');
+    tableRow.setAttribute("class", "divTableRow");
+    tableBody.appendChild(tableRow);
+    var tableCell = document.createElement('div');
+    tableCell.setAttribute("class", "divTableCell");
+    var temp = document.querySelector(".divTableRow")
+    temp.appendChild(tableCell);
+    tableCell.innerText = '1';
+    for(key in allStudentDetails.mtech[0]){
+        var tableCell = document.createElement('div');
+        tableCell.setAttribute('class', "divTableCell");
+        document.querySelector('.divTableRow').append(tableCell); 
+        tableCell.innerHTML = allStudentDetails.mtech[0][key];
+    }
+    console.log(tableRow);
+}
+
 
 
